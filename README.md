d7871b165ceb874e7b233b06468db0844de7a2a9
AGN <itzme907@gmail.com>
2023-10-05 23:09:33 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Shopping-cart
+Built using:
+- **JavaScript**
+- **React**
+- **React Router**
+- **Tailwind Css**  
+
+<br>
+
+\* Part of [The Odin Project](https://www.theodinproject.com/) curriculum
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        'fluid': 'repeat(auto-fit, minmax(200px, 1fr))',
+      },
+      gridTemplateRows: {
+        'fluid': 'repeat(auto-fit, minmax(100px, 1fr))'
+      }
+    },
+  },
+  plugins: [],
+}
+
