7f6fc8df43ab26ff89c21fab8b5865aa85a01a07
AGN <itzme907@gmail.com>
2023-10-09 20:29:23 +0300

    pass down cart items state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import { useState } from "react";
 
 export default function App() {
-    return (
-        <>
-        <Header />
-        <Outlet />
-        </>
-    )
+  const [cartItems, setCartItems] = useState([]);
+  function handleProductAdded(product) {
+    const newCartItems = [...cartItems];
+
+    const pickedItem = newCartItems.find(
+      (item) => item.title === product.title,
+    );
+
+    if (pickedItem) {
+      pickedItem.quantity += product.quantity;
+      setCartItems(newCartItems);
+      return;
+    }
+
+    newCartItems.push(product);
+    setCartItems(newCartItems);
+  }
+
+  const cartProductsCount = cartItems.reduce((accum, item) => {
+    return accum + item.quantity;
+  }, 0);
+
+  return (
+    <>
+      <Header cartLength={cartProductsCount} />
+      <Outlet context={{ handleProductAdded, cartItems, setCartItems }} />
+    </>
+  );
 }
